e5a473a8d0f1bd25c6f9f97ccaa2c0302074c234
Kevin Reblora <kp.reblora@gmail.com>
2023-01-27 17:11:08 +0800

    Fixing k8s deployment script

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -3,11 +3,13 @@
 sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
 kubectl -n default get deployment ${deploymentName} > /dev/null
 
-if [[ $? -ne 0 ]]; then
-    echo "Deployment ${deploymentName} doesn't exist"
-    kubectl apply -f k8s_deployment_service.yaml
-else
-    echo "Deployment ${deploymentName} exist"
-    echo "Image Name - ${imageName} exist"
-    kubectl -n default set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
-fi;
+# if [[ $? -ne 0 ]]; then
+#     echo "Deployment ${deploymentName} doesn't exist"
+#     kubectl apply -f k8s_deployment_service.yaml
+# else
+#     echo "Deployment ${deploymentName} exist"
+#     echo "Image Name - ${imageName} exist"
+#     kubectl -n default set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
+# fi;
+
+kubectl apply -f k8s_deployment_service.yaml
