cbbe37ade131b0cb4e8d0cbb0ee610bbb9a6f9cf
Kevin Reblora <kp.reblora@gmail.com>
2023-01-30 22:52:37 +0800

    Testing slack message

---
+++ vars/sendNotification.groovy
@@ -0,0 +1,24 @@
+#!/usr/bin/env groovy
+
+/**
+ * Send notifications based on build status string
+ */
+def call(String buildStatus = 'STARTED') {
+  // build status of null means successful
+  buildStatus = buildStatus ?: 'SUCCESS'
+
+def color
+
+  if (buildStatus == 'SUCCESS') {
+    color = '#47ec05'
+  } else if (buildStatus == 'UNSTABLE') {
+    color = '#d5ee0d' 
+  } else {
+    color = '#ec2805'
+  }
+
+  // Send notifications
+def msg = "${buildStatus}: '${env.JOB_NAME}' #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
+
+  slackSend(color: color, message: msg)
+}
