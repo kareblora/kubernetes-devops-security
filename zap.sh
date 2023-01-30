203427fe23e3d475d95e01f7bef664b20749ae01
Kevin Reblora <kp.reblora@gmail.com>
2023-01-30 21:46:02 +0800

    Configured files to resolve X content

--- zap.sh
+++ zap.sh
@@ -6,6 +6,8 @@ chmod 777 $(pwd)
 echo $(id -u):$(id -g)
 docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -r zap_report.html
 
+#docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -c zap-rules.txt -r zap_report.html
+
 exit_code=$?
 
 #HTML Report
