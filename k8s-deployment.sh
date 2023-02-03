6c752836cdeca0202e911820fddddf2817bb2c72
Kevin Reblora <kp.reblora@gmail.com>
2023-02-04 00:02:47 +0800

    test

--- k8s-deployment.sh
+++ k8s-deployment.sh
@@ -2,7 +2,7 @@
 
 export KUBECONFIG=/etc/kubernetes/admin.conf
 sudo sed -i "s#replace#${imageName}#g" k8s_deployment_service.yaml
-sudo kubectl -n default get deployment ${deploymentName} > /dev/null
+kubectl -n default get deployment ${deploymentName} > /dev/null
 
 # if [[ $? -ne 0 ]]; then
 #     echo "Deployment ${deploymentName} doesn't exist"
@@ -13,4 +13,4 @@ sudo kubectl -n default get deployment ${deploymentName} > /dev/null
 #     kubectl -n default set image deploy ${deploymentName} ${containerName}=${imageName} --record=true
 # fi;
 
-sudo kubectl apply -f k8s_deployment_service.yaml
+kubectl apply -f k8s_deployment_service.yaml
